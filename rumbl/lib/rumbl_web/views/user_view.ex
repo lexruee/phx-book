4c8ef4436f8166305718f92c792925b5647d0c8f
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-07-23 00:12:51 +0200

    Refactor View and Template

--- rumbl/lib/rumbl_web/views/user_view.ex
+++ rumbl/lib/rumbl_web/views/user_view.ex
@@ -1,3 +1,17 @@
 defmodule RumblWeb.UserView do
   use RumblWeb, :view
+
+  alias Rumbl.Accounts
+
+  def first_name(%Accounts.User{name: name}) do
+    name
+    |> String.split
+    |> hd
+  end
+
+  def last_name(%Accounts.User{name: name}) do
+    name
+    |> String.split
+    |> tl
+  end
 end
