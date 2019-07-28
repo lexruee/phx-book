d874daf977cc8996275624b44d133b5c298447be
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-07-29 01:28:32 +0200

    Add video test

--- rumbl/test/rumbl/multimedia_test.exs
+++ rumbl/test/rumbl/multimedia_test.exs
@@ -17,4 +17,20 @@ defmodule Rumbl.Multimediatest do
       assert alpha_names == ~w[Action Comedy Drama]
     end
   end
+
+  describe "videos" do
+    alias Rumbl.Multimedia.Video
+
+    @valid_attrs %{description: "desc", title: "title", url: "http://local"}
+    @invalid_attrs %{description: nil, title: nil, url: nil}
+
+    test "list_videos/0 returns all videos" do
+      owner = user_fixture()
+      %Video{id: id1} = video_fixture(owner)
+      assert [%Video{id: ^id1}] = Multimedia.list_videos()
+
+      %Video{id: id2} = video_fixture(owner)
+      assert [%Video{id: ^id1}, %Video{id: ^id2}] = Multimedia.list_videos()
+    end
+  end
 end
--- rumbl/test/support/test_helpers.ex
+++ rumbl/test/support/test_helpers.ex
@@ -17,7 +17,7 @@ defmodule Rumbl.TestHelpers do
     user
   end
 
-  def video_fxiture(%User{} = user, attrs \\ %{}) do
+  def video_fixture(%User{} = user, attrs \\ %{}) do
     attrs = Enum.into(attrs, %{
       title: "A Title",
       url: "http://example.com",
