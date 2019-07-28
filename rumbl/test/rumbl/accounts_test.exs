44ab3fcac2c35a0020c8a5bb4da391f5b78b2a73
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-07-29 00:34:25 +0200

    Add further tests for the Accounts context

--- rumbl/test/rumbl/accounts_test.exs
+++ rumbl/test/rumbl/accounts_test.exs
@@ -3,6 +3,7 @@ defmodule Rumbl.AccountsTest do
 
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
+  alias Ecto.Changeset
 
   describe "register_user/1" do
     @valid_attrs %{
@@ -18,6 +19,34 @@ defmodule Rumbl.AccountsTest do
       assert user.name == "User"
       assert user.username == "eva"
       assert [%User{id: ^id}] = Accounts.list_users()
+    end
+
+    test "with invalid data does not insert user" do
+      assert {:error, %Changeset{valid?: false}} = Accounts.register_user(@invalid_attrs)
+      assert Accounts.list_users() == []
+    end
+
+    test "enforces unique usernames" do
+      assert {:ok, %User{id: id}=user} = Accounts.register_user(@valid_attrs)
+      assert {:error, changeset = %Changeset{valid?: false}} = Accounts.register_user(@valid_attrs)
+
+      assert %{username: ["has already been taken"]} = errors_on(changeset)
+    end
+
+    test "does not accept long usernames" do
+      attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
+      {:error, changeset} = Accounts.register_user(attrs)
+
+      assert %{username: ["should be at most 20 character(s)"]} = errors_on(changeset)
+      assert Accounts.list_users() == []
+    end
+
+    test "requires password to be at least 6 chars long" do
+      attrs = Map.put(@valid_attrs, :password, "12345")
+      {:error, changeset} = Accounts.register_user(attrs)
+
+      assert %{password: ["should be at least 6 character(s)"]} = errors_on(changeset)
+      assert Accounts.list_users() == []
     end
   end
 end
