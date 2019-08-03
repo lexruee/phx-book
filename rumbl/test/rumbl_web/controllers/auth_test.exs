eff07259b491cfd9d73f7f9134ad5c45138ec8a9
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2019-08-03 23:55:02 +0200

    Add login and logout tests

--- rumbl/test/rumbl_web/controllers/auth_test.exs
+++ rumbl/test/rumbl_web/controllers/auth_test.exs
@@ -1,6 +1,7 @@
 defmodule RumblWeb.AuthTest do
   use RumblWeb.ConnCase, async: true
   alias RumblWeb.Auth
+  alias Rumbl.Accounts.User
 
   setup %{conn: conn} do
     conn = conn
@@ -17,9 +18,28 @@ defmodule RumblWeb.AuthTest do
 
   test "authenticate_user continues when current_user exists", %{conn: conn} do
     conn = conn
-           |> assign(:current_user, %Rumbl.Accounts.User{})
+           |> assign(:current_user, %User{})
            |> Auth.authenticate_user([])
 
     refute conn.halted
+  end
+
+  test "login puts the user in the session", %{conn: conn} do
+    login_conn = conn
+                 |> Auth.login(%User{id: 123})
+                 |> send_resp(:ok, "")
+
+    next_conn = get(login_conn, "/")
+    assert 123 = get_session(next_conn, :user_id)
+  end
+
+  test "login drops the session", %{conn: conn} do
+    login_conn = conn
+                 |> put_session(:user_id, 123)
+                 |> Auth.logout()
+                 |> send_resp(:ok, "")
+
+    next_conn = get(login_conn, "/")
+    refute get_session(next_conn, :user_id)
   end
 end
